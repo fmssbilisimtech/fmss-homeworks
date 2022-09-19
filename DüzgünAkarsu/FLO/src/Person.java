697f09e10bd791dfa24c122c2057b5c1558bf8ca
Düzgün Akarsu <duzgunakarsu@gmail.com>
2022-09-19 15:57:53 +0300

    DüzgünAkarsu FLO OOP Example

---
+++ DüzgünAkarsu/FLO/src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname=surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
